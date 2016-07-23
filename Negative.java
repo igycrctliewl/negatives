cc1171b1551efcb28e0da71531d23d5613b1b765
SurfacePro4 <yomikebro@gmail.com>
2016-07-22 23:21:42 -0700

    refactored Negative.java as a reusable class

--- Negative.java
+++ Negative.java
@@ -1,6 +1,7 @@
 /**
  * File: Negative.java
- * 
+ * Adapted from an online example as described below
+ *
  * Description:
  * Convert color image to negative.
  * 
@@ -15,52 +16,65 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 public class Negative {
+
+   private File sourceFile;
+
+   public Negative( File file ) {
+      this.sourceFile = file;
+   }
+
    
-   public static void main(String args[])throws IOException{
+   public static void main( String[] args ) throws IOException{
       if(args.length != 1) {
          System.err.println("Usage: Negative jpgFileName");
          System.exit(1);
       }
 
-      String filename = args[0];
+      Negative neg = new Negative( new File( args[0] ));
+      neg.reverseImage();
+   }
 
-      BufferedImage img = null;
-      File f = null;
+
+   public void reverseImage() {
       //read image
-      try{
-        f = new File( filename );
-        img = ImageIO.read(f);
-      }catch(IOException e){
-        System.out.println(e);
+      BufferedImage img = null;
+      try {
+         img = ImageIO.read( this.sourceFile );
+      } catch( IOException e ) {
+         System.out.println( e );
       }
      
       //get image width and height
       int width = img.getWidth();
       int height = img.getHeight();
+
       //convert to negative
-      for(int y = 0; y < height; y++){
-         for(int x = 0; x < width; x++){
-            int p = img.getRGB(x,y);
-            int a = (p>>24)&0xff;
-            int r = (p>>16)&0xff;
-            int g = (p>>8)&0xff;
-            int b = p&0xff;
+      for( int y = 0; y < height; y++ ) {
+         for( int x = 0; x < width; x++ ) {
+            int p = img.getRGB( x, y );
+            int a = ( p >> 24 ) & 0xff;
+            int r = ( p >> 16 ) & 0xff;
+            int g = ( p >> 8 ) & 0xff;
+            int b = p & 0xff;
+
             //subtract RGB from 255
             r = 255 - r;
             g = 255 - g;
             b = 255 - b;
             //set new RGB value
-            p = (a<<24) | (r<<16) | (g<<8) | b;
-            img.setRGB(x, y, p);
+            p = ( a << 24 ) | ( r << 16 ) | ( g << 8 ) | b;
+            img.setRGB( x, y, p );
          }
       }
 
       //write image
+      String fileName = this.sourceFile.getName().substring( 0, this.sourceFile.getName().lastIndexOf(".") );
+      String extension =  this.sourceFile.getName().substring( this.sourceFile.getName().lastIndexOf(".") );
+      File out = new File( fileName + "Output" + extension );
       try {
-         f = new File( filename + "Output.jpg");
-         ImageIO.write(img, "jpg", f);
-      } catch(IOException e) {
-         System.out.println(e);
+         ImageIO.write( img, "jpg", out );
+      } catch( IOException e ) {
+         System.out.println( e );
       }
    }
 }
