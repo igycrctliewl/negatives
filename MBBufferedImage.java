297237c0fd01ae4be984c57a6e402b3e80b5ce75
SurfacePro4 <yomikebro@gmail.com>
2016-07-24 23:04:42 -0700

    completed refactoring; created new class MBBufferedImage to service BufferedImage

--- MBBufferedImage.java
+++ MBBufferedImage.java
@@ -1,104 +1,62 @@
 /**
- * File: Negative.java
- * Adapted from an online example as described below
+ * Adapted from an online example Negative.java as described below
  *
- * Description:
- * Convert color image to negative.
- * 
- * @author Yusuf Shakeel
+ * @author mikebro, after Yusuf Shakeel
  * Date: 27-01-2014 mon
- *
  * www.github.com/yusufshakeel/Java-Image-Processing-Project
  */
-import java.io.File;
-import java.io.IOException;
 import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 
 public class MBBufferedImage {
 
-   private File sourceFile;
+   private BufferedImage img;
 
-   public Negative( File file ) {
-      this.sourceFile = new File( file.getAbsolutePath() );
+   public MBBufferedImage( BufferedImage img ) {
+      this.img = img;
    }
 
-   
-   public static void main( String[] args ) throws IOException{
-      if(args.length != 1) {
-         System.err.println("Usage: Negative jpgFileName");
-         System.exit(1);
-      }
+   public BufferedImage getNegativeImg() {
 
-      Negative neg = new Negative( new File( args[0] ));
-      neg.reverseImage();
-   }
-
-
-   public void reverseImage() {
-      //read image
-      BufferedImage img = null;
-      try {
-         img = ImageIO.read( this.sourceFile );
-      } catch( IOException e ) {
-         System.out.println( e );
-      }
-     
+      // create new copy of this image
+      BufferedImage copy = this.duplicate();
 
       //get image width and height
-      int width = img.getWidth();
-      int height = img.getHeight();
+      int width = copy.getWidth();
+      int height = copy.getHeight();
 
       //convert to negative
       for( int y = 0; y < height; y++ ) {
          for( int x = 0; x < width; x++ ) {
-            int p = img.getRGB( x, y );
+            int p = copy.getRGB( x, y );
             int a = ( p >> 24 ) & 0xff;
             int r = ( p >> 16 ) & 0xff;
             int g = ( p >> 8 ) & 0xff;
             int b = p & 0xff;
-
+   
             //subtract RGB from 255
             r = 255 - r;
             g = 255 - g;
             b = 255 - b;
             //set new RGB value
             p = ( a << 24 ) | ( r << 16 ) | ( g << 8 ) | b;
-            img.setRGB( x, y, p );
+            copy.setRGB( x, y, p );
          }
       }
-
-      //write image
-      String fileName = this.sourceFile.getName().substring( 0, this.sourceFile.getName().lastIndexOf(".") );
-      String extension =  this.sourceFile.getName().substring( this.sourceFile.getName().lastIndexOf(".") );
-      String outputFile = this.sourceFile.getParentFile() + File.separator + "output" + File.separator;
-
-      // Create File object to make directories
-      File out = new File( outputFile );
-      out.mkdirs();
-
-      // Recreate File object with fully qualified file name and write output
-      outputFile = outputFile + fileName + "Inverted" + extension;
-      out = new File( outputFile );
-      try {
-         ImageIO.write( img, "jpg", out );
-      } catch( IOException e ) {
-         System.out.println( e );
-      }
+   
+      return copy;
    }
 
 
-
+   // adapted from a post at:
    // http://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage
-   // public static BufferedImage deepCopy(BufferedImage bi) {
-   //    ColorModel cm = bi.getColorModel();
-   //    boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
-   //    WritableRaster raster = bi.copyData(bi.getRaster().createCompatibleWritableRaster());
-   //    return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
-   // }
-
-
-
+   public BufferedImage duplicate() {
+      ColorModel cm = this.img.getColorModel();
+      boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+      WritableRaster raster = this.img.copyData( this.img.getRaster().createCompatibleWritableRaster() );
+      return new BufferedImage( cm, raster, isAlphaPremultiplied, null );
+   }
 }
 
 
